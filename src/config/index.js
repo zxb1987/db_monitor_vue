0e0e9779b397c9d7c1829c809d6b8f2799051abe
zhangxingbo <215957956@qq.com>
2020-03-17 10:52:27 +0800

    更新

--- src/config/index.js
+++ src/config/index.js
@@ -16,7 +16,7 @@ export default {
   /**
    * @description api请求基础路径
    */
-  baseUrl: {
+  publicPath: {
     // dev: 'https://www.easy-mock.com/mock/5add9213ce4d0e69998a6f51/iview-admin/',
     // dev: 'http://114.116.16.6:9991/',
     // pro: 'http://114.116.16.6:9991/'
